c1973077eb653f244787a7c1e57cc4e2f95ba8b4
DylanConvery <dylanconvery94@gmail.com>
2022-02-15 12:06:57 +0000

    initial commit

---
+++ Bagel/Bagel.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Bagel
+{
+    public class Bagel : Game
+    {
+        private GraphicsDeviceManager m_graphics;
+        private SpriteBatch m_spriteBatch;
+
+        public Bagel()
+        {
+            m_graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            m_spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void Update(GameTime game_time)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                Exit();
+            }
+
+            base.Update(game_time);
+        }
+
+        protected override void Draw(GameTime game_time)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            base.Draw(game_time);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Bagel
+
+![Placeholder Bagel](http://pixelartmaker-data-78746291193.nyc3.digitaloceanspaces.com/image/11596cfdff669a3.png)
+
+## About
+
+Bagel is a cross-platform video game about about bagel who must take it upon itself to save all the people it can from the raging inferno engulfing its hole apartment block.
+
+## Technology 
+
+Framework: MonoGame
