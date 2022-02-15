0289f74cf926c7a49ecdb46a08bdf745799437b0
DylanConvery <dylanconvery94@gmail.com>
2022-02-15 23:50:20 +0000

    made start on entity component system

---
+++ Bagel.Core/Source/Component.cs
@@ -0,0 +1,11 @@
+﻿namespace Bagel {
+    public abstract class Component {
+        protected Component(GameObject parent) {
+            m_parent_game_object = parent;
+        }
+        public abstract void Update();
+        public abstract void Render();
+
+        private GameObject m_parent_game_object;
+    }
+}
---
+++ Bagel.Core/Source/GameObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace Bagel {
+    public class GameObject {
+        public GameObject(){}
+        public void Update(){}
+        public void Render(){}
+
+        //add component
+        //remove component
+        //get component
+        //remove all components
+
+        private Vector2 m_position;
+        private int m_layer_index;
+        private string m_name;
+        private uint m_id;
+        private bool m_enabled;
+        private List<Component> m_components;
+    }
+}
