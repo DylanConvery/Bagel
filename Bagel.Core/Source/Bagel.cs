5eef5166295cf8207d2f3f852d251fb5117700e6
DylanConvery <dylanconvery94@gmail.com>
2022-03-12 13:22:20 +0000

    opted for using DefaultECS instead of writing my own implementation for times sake

--- Bagel.Core/Source/Bagel.cs
+++ Bagel.Core/Source/Bagel.cs
@@ -2,52 +2,40 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using DefaultEcs;
+using DefaultEcs.System;
 
 namespace Bagel {
     public class Bagel : Game {
         private GraphicsDeviceManager m_graphics;
         private SpriteBatch m_sprite_batch;
-        private GameObject cube;
-        private Texture2D texture;
-        private Color[] colorData;
+        private World m_world;
+        private ISystem<SpriteBatch> _drawSystem;
 
         public Bagel() {
             m_graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            IsFixedTimeStep = false;
         }
 
-        protected override void Initialize() {
-            texture = new Texture2D(GraphicsDevice, 100, 100);
-            colorData = new Color[100 * 100];
-            cube = new GameObject("cube");
+        protected   override void Initialize() {
             base.Initialize();
         }
 
         protected override void LoadContent() {
             m_sprite_batch = new SpriteBatch(GraphicsDevice);
-            for (int i = 0; i < colorData.Length; i++) {
-                colorData[i] = Color.Red;
-            }
-            texture.SetData(colorData);
-            cube.AddComponent<Sprite>(texture);
         }
 
         protected override void Update(GameTime game_time) {
             if (Keyboard.GetState().IsKeyDown(Keys.Escape)) {
                 Exit();
             }
-
-            cube.Update(game_time);
-            cube.Position += new Vector2(0.2f * (float)game_time.ElapsedGameTime.TotalMilliseconds, 0);
             base.Update(game_time);
         }
 
         protected override void Draw(GameTime game_time) {
             GraphicsDevice.Clear(Color.Black);
-            m_sprite_batch.Begin();
-            cube.Draw(m_sprite_batch);
-            m_sprite_batch.End();
             base.Draw(game_time);
         }
     }
